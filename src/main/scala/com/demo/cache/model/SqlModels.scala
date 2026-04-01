dabc516d77da5f52e1bb1f4de6eea6ff9061c390
bitcoin <67271469+bitcoin-eth@users.noreply.github.com>
2026-04-01 16:30:07 +0800

    Add SQL analysis models

---
+++ src/main/scala/com/demo/cache/model/SqlModels.scala
@@ -0,0 +1,18 @@
+package com.demo.cache.model
+
+case class QueryUnit(
+  unitId: String,
+  sourceType: String,
+  objectName: Option[String],
+  rawText: String,
+  queryText: String
+)
+
+case class GlobalRepeatedCandidate(
+  fingerprint: String,
+  occurrences: Int,
+  nodeSize: Int,
+  sourceUnitIds: Seq[String],
+  sourceObjectNames: Seq[String],
+  examplePlanText: String
+)
