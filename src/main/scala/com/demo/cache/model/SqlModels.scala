44cc2d23bee2ed2f3c8aaf82dc7a18970727536c
bitcoin <67271469+bitcoin-eth@users.noreply.github.com>
2026-04-02 15:54:59 +0800

    Update SqlModels.scala

--- src/main/scala/com/demo/cache/model/SqlModels.scala
+++ src/main/scala/com/demo/cache/model/SqlModels.scala
@@ -8,11 +8,19 @@ case class QueryUnit(
   queryText: String
 )
 
+case class CandidateSourceSql(
+  unitId: String,
+  objectName: Option[String],
+  rawText: String,
+  queryText: String
+)
+
 case class GlobalRepeatedCandidate(
   fingerprint: String,
   occurrences: Int,
   nodeSize: Int,
   sourceUnitIds: Seq[String],
   sourceObjectNames: Seq[String],
-  examplePlanText: String
+  examplePlanText: String,
+  sourceSqls: Seq[CandidateSourceSql]   
 )
